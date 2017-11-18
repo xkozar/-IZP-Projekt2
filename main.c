5ecfb7ceb2064031d55ce770f73906126bc83eab
xkozar <32489502+xkozar@users.noreply.github.com>
2017-11-18 15:28:25 +0100

    Commit

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 /**********************************************************
  * proj2 Iteracne vypocty
  * author: Tomas Kozar xkozar02
- * last edited: 14.november 2017
+ * last edited: 17.november 2017
  **********************************************************/
 
 #include <stdio.h>
@@ -120,18 +120,26 @@ void printValues(double degree, int iterations){
  * @param [in] degree used for caltulations of tangens
  * @param [in] N first number of iterations
  * @param [in] M last number of iterations
+ * @return 0 if method ran succesfully, 1 if function found error
  */
-void compareTanCalcs(double degree, int N, int M){
-   if(M > MAXITERATIONS)
+int compareTanCalcs(double degree, int N, int M){
+   if(M > MAXITERATIONS) {
       fprintf(stderr, "Arguement M too high");
+      return 1;
+   }
    if(N < 1 || M < N) {
       fprintf(stderr, "Wrong values of arguements N and/or M");
-      exit(1);
+      return 1;
+   }
+   else if(N < 1){
+      fprintf(stderr, "Arguement N must be > than 0");
+      return 1;
    }
    printf("%.10e\n", degree);
    for(int i = N; i <= M; i++){
       printValues(degree, i);
    }
+   return 0;
 }
 
 /**
@@ -142,26 +150,40 @@ void compareTanCalcs(double degree, int N, int M){
  * @param [in] betaIncluded tells if we also do calculation for beta
  */
 
-void measurement(double alpha, double beta, double deviceHeight, bool betaIncluded){
+int measurement(double alpha, double beta, double deviceHeight, bool betaIncluded){
+   if(alpha <= 0 || alpha > 1.4){
+      fprintf(stderr, "Invalid value of arguement A");
+      return 1;
+   }
+   if(betaIncluded && (beta <= 0 || beta > 1.4)){
+      fprintf(stderr, "Invalid value of arguement B");
+      return 1;
+   }
+   if(deviceHeight <= 0 || deviceHeight > 100){
+      fprintf(stderr, "Invalid value of arguement C");
+      return 1;
+   }
+
    double d = deviceHeight/cfrac_tan(alpha, MEASUREITERATIONS);
    printf("%.10e\n", d);
 
    if(betaIncluded){
       printf("%.10e\n", (d*cfrac_tan(beta, MEASUREITERATIONS)) + deviceHeight);
    }
+   return 0;
 }
 
 /**
  * Checks and evaluate in-line arguments used for measurement
  * @param [in] argc number of arguments
  * @param [in] argv 2d array of chars
+ * @return 0 if method ran succesfully, 1 if function found error
  */
-void checkMeasurementArguments(int argc, char *argv[]){
+int checkMeasurementArguments(int argc, char *argv[]){
    double alpha;
    double beta = 0.;
    double deviceHeight = DEFAULTHEIGHT;
    bool betaIncluded = false;
-   //printf("%d", strcmp(argv[1], "-m"));
    if(argc == 6 && !strcmp(argv[1], "-c") && !strcmp(argv[3], "-m")){
       if(stringIsFloat(argv[2]) && stringIsFloat(argv[4]) && stringIsFloat(argv[5])){
          alpha = atof(argv[4]);
@@ -171,7 +193,7 @@ void checkMeasurementArguments(int argc, char *argv[]){
       }
       else{
          fprintf(stderr, "Wrong argument/s\n");
-         exit(1);
+         return 1;
       }
    }
    else if(argc == 5 && !strcmp(argv[1], "-c") && !strcmp(argv[3], "-m")){
@@ -181,7 +203,7 @@ void checkMeasurementArguments(int argc, char *argv[]){
       }
       else{
          fprintf(stderr, "Wrong argument/s\n");
-         exit(1);
+         return 1;
       }
    }
    else if(argc == 4 && !strcmp(argv[1], "-m")&& stringIsFloat(argv[2]) && stringIsFloat(argv[3])){
@@ -194,27 +216,58 @@ void checkMeasurementArguments(int argc, char *argv[]){
    }
    else{
       fprintf(stderr, "Wrong argument/s\n");
-      exit(1);
+      return 1;
    }
-   measurement(alpha, beta, deviceHeight, betaIncluded);
+
+   if(measurement(alpha, beta, deviceHeight, betaIncluded))
+      return 1;
+
+   return 0;
 }
 
 int main(int argc, char *argv[]) {
 
    if(argc == 2 && !strcmp(argv[1], "--help")) {
-      printf("Napoveda coming soon!!!\n");
+      printf("Program is used to calculate height and distance of measured object using implemented tangens functions.\n"
+                     "Running program: \n"
+                     "--help -> Prints guide for program use.\n"
+                     "-----------------------------------------------------------------------\n"
+                     "--tan A N M -> Prints values of calculated tangenses and their coparision to tangens from math library in defined range of iterations.\n"
+                     "Arguements:\n"
+                     "A -> Angle of used for calculations in radians.\n"
+                     "N -> Start of iteration range.\n"
+                     "M -> End of iteration range.   0 < N <= M < 14\n"
+                     "Output:\n"
+                     "I M T TE C CE\n"
+                     "I -> Iterations used for calculation.\n"
+                     "M -> Tangens calculated using math library.\n"
+                     "T -> Tangens calculated using Taylor's polynome.\n"
+                     "TE -> Absolute difference between M and T\n"
+                     "C -> Tangens calculated using continued fraction.\n"
+                     "CE -> Absolute difference between M and C\n"
+                     "-------------------------------------------------------\n"
+                     "[-c X] -m A [B] -> Prints calculated distance of measured object\n"
+                     "Arguements:\n"
+                     "-c -> [Optional] Sets height of measuring device.\n"
+                     "X -> [Optional] Value of height in meters. Implicitely 1.5.\n"
+                     "-m -> Calculates distance of object.\n"
+                     "A -> Angle of alpha used for calculations in radians.\n"
+                     "B -> [Optional] Value of beta in radians used for calculation of objects height.");
       return 0;
    }
    else if(argc == 5 && !strcmp(argv[1], "--tan")) {
-      if(stringIsFloat(argv[2]) && stringIsNumber(argv[3]) && stringIsNumber(argv[4]))
-         compareTanCalcs(atof(argv[2]), atoi(argv[3]), atoi(argv[4]));
+      if(stringIsFloat(argv[2]) && stringIsNumber(argv[3]) && stringIsNumber(argv[4])) {
+         if (compareTanCalcs(atof(argv[2]), atoi(argv[3]), atoi(argv[4])) == 1)
+            return 1;
+      }
       else {
          fprintf(stderr, "Wrong format of arguement/s A,N and/or N");
          return 1;
       }
    }
    else if(argc == 3 || argc == 4 || argc == 6){
-      checkMeasurementArguments(argc, argv);
+      if(checkMeasurementArguments(argc, argv) == 1)
+         return 1;
    }
    else {
       fprintf(stderr, "Wrong argument/s\n");
